d90b6182b92668f65ea548e3ffde712ee7336c96
Bohdan Bobrowski <bohdan@bobrowski.com.pl>
2022-02-11 01:19:21 +0100

    black refacor

--- setup.py
+++ setup.py
@@ -5,22 +5,20 @@ from setuptools import setup
 
 
 setup(
-    name='mdmc2exif',
-    version='1.1',
-    description=u'Simple script to add specific ImageDescription EXIF tag to images, based on data stored in csv file.',
+    name="mdmc2exif",
+    version="1.1",
+    description="Simple script to add specific ImageDescription EXIF tag to images, based on data stored in csv file.",
     url="https://github.com/bohdanbobrowski/mdmc2exif",
     author="Bohdan Bobrowski",
     author_email="bohdanbobrowski@gmail.com",
     license="MIT",
-    packages=[
-        "mdmc2exif"
-    ],
+    packages=["mdmc2exif"],
     install_requires=[
         "piexif",
     ],
     entry_points={
-        'console_scripts': [
-            'mdmc2exif = mdmc2exif.mdmc2exif:main',
+        "console_scripts": [
+            "mdmc2exif = mdmc2exif.mdmc2exif:main",
         ]
     },
 )
